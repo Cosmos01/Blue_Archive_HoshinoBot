d69efa6f1ea30afed59816bd410c3ba7b477a35f
Cosmos <37209685+Cosmos01@users.noreply.github.com>
2023-01-21 04:35:09 +0800

    Update raid_img.py

--- raid_img.py
+++ raid_img.py
@@ -3,9 +3,9 @@ from hoshino import aiorequests
 
 async def get_img(server = "日"):
     if server == "國際":
-        url = "http://45.86.70.253:40000/ba_raid_global.json"
+        url = "http://91.149.236.232:40000/ba_raid_global.json"
     else:
-        url = "http://45.86.70.253:40000/ba_raid_jp.json"
+        url = "http://91.149.236.232:40000/ba_raid_jp.json"
     try:
         res = await aiorequests.get(url,timeout=20)
         json_data = await res.json()
